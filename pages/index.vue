09f40b1f948a64c0ab64591a1595d046825b30b6
Ryoma <r.koike@creativehope.jp>
2020-05-06 16:44:26 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <pre>{{now}}</pre>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: 'hello',
+      message: 'hello too',
+      now: 'wait.....'
+    };
+  },
+  created: function() {
+    setInterval (()=>{
+      var d = new Date();
+      this.now = d.getHours()
+          + ':' +  d.getMinutes()
+          + ':' +  d.getSeconds();
+    }, 1000);
+  }
+}
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+
+h1 {
+  font-size: 60px;
+}
+
+p {
+  padding-top: 5px;
+  font-size: 20px;
+}
+
+pre {
+  padding: 10px;
+  font-size: 10px;
+  background-color: #efefef;
+}
+
+hr {
+  margin:10px 0px; 
+}
+
+
+</style>
---
+++ pages/other.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="container">
+        <h1>{{title}}</h1>
+        <p>{{message}}</p>
+        <hr>
+        <router-link to="/">Go to Top</router-link>
+    </section>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            title:'Other',
+            message:'これは、別のページ',
+        };
+    },
+}
+</script>
+
+<style>
+.container {
+    padding: 5px 10px;
+}
+
+h1 {
+    font-size:60p; 
+    color:#345980
+}
+
+p {
+    padding-top: 2px;
+    font-size: 20px;
+}
+
+a {
+    font-size: 10px;
+}
+</style>
